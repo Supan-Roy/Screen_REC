d8e7f57ed922b624ba7f167385fa67f56eac79ef
Supan Roy <supanroy2021@gmail.com>
2025-06-05 17:53:54 +0600

    Initial commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import datetime
+
 from PIL import ImageGrab
 import numpy as np
 import cv2
@@ -5,7 +7,8 @@ from win32api import GetSystemMetrics
 
 width = GetSystemMetrics(0)
 height = GetSystemMetrics(1)
-
+time_stamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+print(time_stamp)
 fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 captured_video = cv2.VideoWriter('screen_rec.mp4', fourcc, 24.0, (width, height))
 
@@ -17,4 +20,5 @@ while True:
     img_final = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
     cv2.imshow("Screen Capture", img_final)
     captured_video.write(img_final)
-    cv2.waitKey(10)
+    if cv2.waitKey(10) == ord('q'):
+        break
